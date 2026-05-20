85ea18b686de5310e4466eca8fe96eef057fded6
TpMax6955 <maximilliamga39@gmail.com>
2026-05-19 21:28:11 -0500

    Interfaz de login creada con Qt5

---
+++ Tercer_Corte/Proyecto_Final/build/CMakeFiles/HealthSync_ALSE.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HealthSync_ALSE_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/HealthSync_ALSE_autogen.dir/ParseCache.txt"
+  "HealthSync_ALSE_autogen"
+  "CMakeFiles/HealthSync_ALSE.dir/HealthSync_ALSE_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/HealthSync_ALSE.dir/HealthSync_ALSE_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/HealthSync_ALSE.dir/src/DatabaseManager.cpp.o"
+  "CMakeFiles/HealthSync_ALSE.dir/src/DatabaseManager.cpp.o.d"
+  "CMakeFiles/HealthSync_ALSE.dir/src/Login.cpp.o"
+  "CMakeFiles/HealthSync_ALSE.dir/src/Login.cpp.o.d"
+  "CMakeFiles/HealthSync_ALSE.dir/src/main.cpp.o"
+  "CMakeFiles/HealthSync_ALSE.dir/src/main.cpp.o.d"
+  "HealthSync_ALSE"
+  "HealthSync_ALSE.pdb"
+  "HealthSync_ALSE_autogen/mocs_compilation.cpp"
+  "HealthSync_ALSE_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HealthSync_ALSE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tercer_Corte/Proyecto_Final/build/CMakeFiles/HealthSync_ALSE_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HealthSync_ALSE_autogen"
+  "HealthSync_ALSE_autogen/mocs_compilation.cpp"
+  "HealthSync_ALSE_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/HealthSync_ALSE_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
